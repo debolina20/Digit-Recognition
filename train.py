2f8c93c50088e8e21bc3ac893836ffe466f7468f
Debolina Das <66356392+debolina20@users.noreply.github.com>
2023-11-12 13:03:07 -0600

    Update train.py

--- train.py
+++ train.py
@@ -5,18 +5,13 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
-# the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 print(x_train.shape, y_train.shape)
-
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
-
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
-
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -27,7 +22,7 @@ print(x_test.shape[0], 'test samples')
 
 batch_size = 128
 num_classes = 10
-epochs = 3
+epochs = 10
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5),activation='relu',input_shape=input_shape))
@@ -40,7 +35,6 @@ model.add(Dropout(0.3))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
-
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
 hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
